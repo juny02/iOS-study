d0d8cb65341d1a55c7c3bdecc33e4213198c44df
김정희 <2020006208@hanyang.ac.kr>
2023-09-27 16:32:20 +0900

    2

---
+++ week1. Platform/week1 실습/week1 실습/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week1 실습
+//
+//  Created by 김정희 on 2023/09/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
